f9121f48cc62950df8a996a078621872e34c2e6a
Esther Maina <mainaesther77@gmial.com>
2022-10-28 07:18:51 +0300

    Completed assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
+#
+class Book
+    attr_reader :title
+  
+    def initialize(title, author_name, page_count, genre)
+        @title = title
+        @author_name = author_name
+        @page_count = page_count
+        @genre = genre
+    end
+  
+end
 
+fido = Dog.new("Fido", "Pug")
+fido.breed
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,13 @@
 # Make your shoe class here!
+class shoe
+    attr_reader :brand
+  
+    def initialize(brand, color, size, material, condition)
+        @brand = brand
+        @color = color
+        @size = size
+        @material = material
+        @condition = condition
+    end
+  
+end
